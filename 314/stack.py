237ae91ec88968d47b569c32d73a4720a31e0c1b
Colin Knox <colin.r.knox@gmail.com>
2022-03-14 18:39:42 -0500

    cleaned up and added errors

--- 314/stack.py
+++ 314/stack.py
@@ -4,20 +4,21 @@ class Stack:
         self.max_stack = []
         
     def push(self, elem):
-        if not self.max_stack:
-            self.max_stack.append(elem)
-        if elem >= self.get_max():
+        if not self.data or elem >= self.get_max():
             self.max_stack.append(elem)
         self.data.append(elem)
         
     def pop(self):
-        result = self.data.pop()
-        if result == self.get_max():
+        if not self.data:
+            raise Exception('Stack is empty')
+        if self.data[-1] == self.get_max():
             self.max_stack.pop()
-        return result
+        return self.data.pop()
         
     def get_max(self):
-        return self.max_stack[-1]
+        if not self.data:
+            raise Exception('Stack is empty')
+        return self.data[-1] if not self.max_stack else self.max_stack[-1]
     
 my_stack = Stack()
 
@@ -29,4 +30,6 @@ print(my_stack.get_max())
 my_stack.pop()
 my_stack.pop()
 print(my_stack.get_max())
-        
+my_stack.pop()
+# my_stack.pop() -- raises exception
+my_stack.get_max()
