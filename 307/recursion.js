35d483902f654c360f21f5cc1cee7853d41b858f
Colin Knox <colin.r.knox@gmail.com>
2022-03-17 14:05:06 -0500

    added documentation to the recursive JS

--- 307/recursion.js
+++ 307/recursion.js
@@ -1,6 +1,11 @@
 function getItemsAt(arr, pos) {
     let flag = false;
 
+    /**
+     * Need a nested self invoking function to keep the flag across recursions
+     * We ideally don't want flag to be global so using the functional scope is
+     * the next best thing
+     */
     return (function getItemsAtHelper(arr, pos) {
         if (pos == "even" && arr.length == 2) {
             arr.splice(1)
